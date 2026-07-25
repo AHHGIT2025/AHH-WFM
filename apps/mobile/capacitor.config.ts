01063022c8152d52cdc5e9be1e9ddf6677cf86cd
AHHGIT2025 <itadmin@alhattabholding.com>
2026-07-25 15:49:14 +0300

    feat(mobile): configure internal android test apk

--- apps/mobile/capacitor.config.ts
+++ apps/mobile/capacitor.config.ts
@@ -2,17 +2,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'qa.com.alhattab.ahhwfm',
-  appName: 'AHH WFM',
-  webDir: 'out',
-
+  appName: 'AHH WFM Mobile',
+  webDir: 'www',
   server: {
     url: 'http://10.10.50.24:3201',
     cleartext: true,
-    allowNavigation: [
-      '10.10.50.24',
-    ],
   },
-
   android: {
     allowMixedContent: true,
   },
